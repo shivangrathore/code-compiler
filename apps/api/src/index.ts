3d06f32f3ce012464d8c26c44c5ce3ee75ba6466
Shivang <rshivang12345@gmail.com>
2025-01-31 18:49:24 +0530

    feat: reimagine router

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { handler as authHandler } from "@repo/auth";
 import "dotenv/config";
-import { router as runnerRouter } from "./routes/runner";
+import { router as apiRouter } from "./routes";
 
 const app = express();
 
@@ -9,11 +9,7 @@ const app = express();
 app.use("/api/auth", authHandler);
 
 app.use(express.json());
-app.use(runnerRouter);
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
-});
-
+app.use("/api", apiRouter);
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
