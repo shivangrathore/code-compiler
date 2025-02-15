a5e028f3f47d1483220ddc9df0e013229aee0248
Shivang <rshivang12345@gmail.com>
2025-02-15 22:25:17 +0530

    chore: update base domain

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://api.cc.seekhcode.me/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
