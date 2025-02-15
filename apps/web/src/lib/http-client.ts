b03a6b877db241e5a0a7cdb4a9c3a63a21d3f469
Shivang <rshivang12345@gmail.com>
2025-02-15 22:52:48 +0530

    feat: prod ready

--- apps/web/src/lib/http-client.ts
+++ apps/web/src/lib/http-client.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-// export default axios.create({ baseURL: "https://cc.seekhcode.me/api" });
-export default axios.create({ baseURL: "http://localhost:3000/api" });
+export default axios.create({ baseURL: "https://cc.seekhcode.me/api" });
+// export default axios.create({ baseURL: "http://localhost:3000/api" });
