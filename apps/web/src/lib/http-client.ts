e2d8be7e2eb9749acb1aab05487d1d1ac3138920
Shivang <rshivang12345@gmail.com>
2025-02-17 16:53:57 +0530

    fix: env variable name

--- apps/web/src/lib/http-client.ts
+++ apps/web/src/lib/http-client.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log(process.env.NEXT_PUBLIC_URL);
-const BASE_URL = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
 export default axios.create({ baseURL: new URL("/api", BASE_URL).toString() });
