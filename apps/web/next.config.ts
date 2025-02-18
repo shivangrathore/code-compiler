f8caa67723f232b371fac127fcc82793e02c224b
Shivang <rshivang12345@gmail.com>
2025-02-18 15:35:01 +0530

    feat: use standalone nextjs server

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   rewrites: async () => {
     const proxyPath = "/api/:path*";
     const apiDestUrl = new URL(proxyPath, process.env.API_URL);
